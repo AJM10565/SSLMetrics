10085e46d3044e07758083e74b7d7c00e67a81f9
Jonathan <warkentin.jon@gmail.com>
2019-11-18 18:08:02 -0600

    add data_vis.py

---
+++ data_vis.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+y_pos = np.arange(len(objects))
+performance = [10, 8, 6, 4, 2, 1]
+
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Usage')
+plt.title('Programming language usage')
+
+plt.show()
