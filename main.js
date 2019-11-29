ae21f346c477df54d3589af2ad572696295217e5
Jonathan <warkentin.jon@gmail.com>
2019-11-29 17:31:24 -0600

    make electron hello world app

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
