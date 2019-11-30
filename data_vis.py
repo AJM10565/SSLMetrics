e45a4553291c2c678fe98e336ab3a54f56efba4a
Jonathan <warkentin.jon@gmail.com>
2019-11-29 19:39:14 -0600

    make electron app executable by python script

--- data_vis.py
+++ data_vis.py
@@ -1,13 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import subprocess
 
-objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-y_pos = np.arange(len(objects))
-performance = [10, 8, 6, 4, 2, 1]
-
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Usage')
-plt.title('Programming language usage')
-
-plt.show()
+subprocess.call("./data-vis-win32-x64/data-vis.exe")
