5b68c94fde14ec77f90fc430c94d99f962687288
Jonathan <warkentin.jon@gmail.com>
2019-12-10 23:00:56 -0600

    add logic to ChartUtil.js to make line graph from sample data dynamically

--- ChartUtil.js
+++ ChartUtil.js
@@ -1,37 +1,85 @@
 const { Chart } = require("chart.js")
+const csv = require('csv-parser');
+const fs = require('fs');
+
+let rawData = []
+let headers = [];
+let dates = [];
+
+fs.createReadStream('SSLMetricsFakeData.csv')
+    .pipe(csv())
+    .on('headers', (headersIn) => {
+        headers = headersIn;
+        console.log(headers);
+    })
+    .on('data', (row) => {
+        console.log(row);
+        rawData.push(row);
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+    });
+
+for (var i = 0; i < rawData.length; i++) {
+    dates.push(rawData[i]['Date']);
+    rawData[i]['Date'] = null;
+}
+
+
+let metrics = [];
+for (var i = 1; i < headers.length; i++) {
+    metrics[i - 1] = [];
+    for (var j = 0; j < rawData.length; j++) {
+        metrics[i - 1].push(rawData[j][headers[i]]);
+    }
+}
+
+headers.shift();
+
+let chartDatasets = [];
+for (var i = 0; i < metrics.length; i++) {
+    chartDatasets.push({
+        label: headers[i],
+        data: metrics[i],
+        fill: false,
+    });
+}
+
 
 var ctx = document.getElementById('main-chart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: dates,
+        datasets: chartDatasets
     },
     options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'SSL Metrics Fake Data'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
         scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Date'
+                }
+            }],
             yAxes: [{
-                ticks: {
-                    beginAtZero: true
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
                 }
             }]
         }
