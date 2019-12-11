faf50b89ae60ac74dd85bac3a9ff708163f831bc
Jonathan <warkentin.jon@gmail.com>
2019-12-10 21:48:51 -0600

    change string equality check

--- data_vis.py
+++ data_vis.py
@@ -4,13 +4,13 @@ import platform
 # Get operating system of current platform
 host_os = platform.system()
 
-if host_os is "Darwin":
+if host_os == "Darwin":
     # Replace arg in following line with path to .app file on Mac
     # subprocess.call("")
     print("I'm a Mac!")
-elif host_os is "Linux":
+elif host_os == "Linux":
     # Replace arg in following line with path to executable file on Linux
     # subprocess.call("")
     print("I run Linux!")
-else:
+elif host_os == "Windows":
     subprocess.call("./data-vis-win32-x64/data-vis.exe")
