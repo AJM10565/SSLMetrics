0709c6ffed1ca90dd71e2ce2f6d1230d5a3ac3f6
Jonathan <warkentin.jon@gmail.com>
2019-12-11 10:17:02 -0600

    make chart work

--- ChartUtil.js
+++ ChartUtil.js
@@ -2,7 +2,7 @@ const { Chart } = require("chart.js")
 const csv = require('csv-parser');
 const fs = require('fs');
 
-let rawData = []
+let rawData = {}
 let headers = [];
 let dates = [];
 
@@ -11,77 +11,76 @@ fs.createReadStream('SSLMetricsFakeData.csv')
     .on('headers', (headersIn) => {
         headers = headersIn;
         console.log(headers);
+        for (var i = 0; i < headers.length; i++) {
+            rawData[headers[i]] = []
+        }
+        console.log(rawData);
     })
     .on('data', (row) => {
         console.log(row);
-        rawData.push(row);
+        for (var i = 0; i < headers.length; i++) {
+            rawData[headers[i]].push(row[headers[i]])
+        }
     })
     .on('end', () => {
         console.log('CSV file successfully processed');
-    });
+        console.log(rawData);
 
-for (var i = 0; i < rawData.length; i++) {
-    dates.push(rawData[i]['Date']);
-    rawData[i]['Date'] = null;
-}
+        dates = rawData['Date'];
+        delete rawData['Date'];
 
+        console.log("Dates:");
+        console.log(dates);
 
-let metrics = [];
-for (var i = 1; i < headers.length; i++) {
-    metrics[i - 1] = [];
-    for (var j = 0; j < rawData.length; j++) {
-        metrics[i - 1].push(rawData[j][headers[i]]);
-    }
-}
-
-headers.shift();
-
-let chartDatasets = [];
-for (var i = 0; i < metrics.length; i++) {
-    chartDatasets.push({
-        label: headers[i],
-        data: metrics[i],
-        fill: false,
-    });
-}
-
-
-var ctx = document.getElementById('main-chart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: dates,
-        datasets: chartDatasets
-    },
-    options: {
-        responsive: true,
-        title: {
-            display: true,
-            text: 'SSL Metrics Fake Data'
-        },
-        tooltips: {
-            mode: 'index',
-            intersect: false,
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Date'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
+        let chartDatasets = [];
+        for (metric in rawData) {
+            chartDatasets.push({
+                label: metric,
+                data: rawData[metric],
+                fill: false,
+            });
         }
-    }
-});
+
+        console.log("Chart Datasets:")
+        console.log(chartDatasets)
+
+        var ctx = document.getElementById('main-chart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: chartDatasets
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'SSL Metrics Fake Data'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        });
+    });
