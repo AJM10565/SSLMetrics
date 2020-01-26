dc459c7e6cd6e854b6a3543ac06c237c92b1dd52
Jonathan <warkentin.jon@gmail.com>
2020-01-26 13:01:28 -0600

    change axios call in fe server to promise structure, hit backend api on server start, have separate route for index and for retrieved data; enable CORS on backend server

--- backend_app/app.py
+++ backend_app/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 # Flask server that hosts an API route for the frontend to hit, which returns metrics
+# To start, run the following commands:
+# pip install flask
+# pip install flask-cors
+# python app.py
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def hello_world():
-    return 'Hello, Backend!'
+    return 'This is the response from the backend'
 
 
 if __name__ == '__main__':
--- frontend_app/app.js
+++ frontend_app/app.js
@@ -1,10 +1,30 @@
+const axios = require("axios");
+
 var express = require("express");
 var app = express();
+
+let data = "Data not yet retrieved from backend";
+const url = "http://127.0.0.1:5000/"
 
 app.get("/", (req, res, next) => {
     res.send("Hello, Frontend!");
 });
 
+app.get("/data", (req, res, next) => {
+    res.send(data);
+});
+
+const getData = async url => {
+    axios.get(url)
+        .then((response) => {
+            data = response.data;
+            console.log(data);
+        }, (error) => {
+            console.log(error);
+        });
+}
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+    getData(url);
 });
