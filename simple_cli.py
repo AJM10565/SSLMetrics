35cec115143da398c0671dea33dea966002790f2
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-01-29 16:25:21 -0600

    Refactored simple_cli
    Implemented a program function that allows for the user to input their own personal access and repository URL as arguments, reduced complexity of the code, removed extraneous imports, and added some documentation.

--- simple_cli.py
+++ simple_cli.py
@@ -1,36 +1,30 @@
 import sys
-import numpy as np
-import pandas as pd
 import Master
 import sqlite_database
 
-# code takes a single git repo as input and generates an out.csv file
-arg_length = len(sys.argv)
-if arg_length >= 3:
-  print("You entered: " + str(len(sys.argv)) + "arguments, which is too many")
-  print("This takes a single public www.github.com/name/repo/ as input")
-  sys.exit()
+def program():
+	numberOfArgs = len(sys.argv)	# sys.argv[1] = URL, #sys.argv[2] = token
+	
+	if numberOfArgs > 3:
+		print("""ERROR: Too many arguements entered.
+Accepted arguements: GitHub Repository URL, GitHub personal Access Token""")
+		sys.exit(1)
+	
+	elif numberOfArgs != 3:
+		print("""ERROR: Not enough arguements entered.
+Accepted arguements: GitHub Repository URL, GitHub personal Access Token""")
+		sys.exit(1)
+	else:
+		url = sys.argv[1]
+		token = sys.argv[2]
 
-print("Generating data on: " + sys.argv[1])
+		print("Generating data on: " + url)
+		
+		splitURL = url.split("/")
+		username = splitURL[1]
+		repository = splitURL[2]
 
-# Getting the user and repo_name from the input
-f_loc = sys.argv[1][19:].find("/")
-username = sys.argv[1][19:19+f_loc]
-repo_name = sys.argv[1][20+f_loc:]
+		cursor, conn = sqlite_database.open_connection(repository)	# Unsure of what this code does due to lack of knowledge on how the database works
+		Master.central(username, repository, token, cursor, conn)
 
-cursor, conn = sqlite_database.open_connection(repo_name)
-
-Master.central(username, repo_name, cursor, conn)
-
-# Create the pandas DataFrame
-# df = pd.DataFrame(data, columns=['Date', '# of Commits', '# of Issues', '# of Pull Requests'])
-
-# print dataframe.
-# print(df)
-
-
-
-
-
-
-
+	sys.exit(0)
