e4b4ce8d1b9c79729833664d75b875e97d37613d
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-01-29 22:12:58 -0600

    Added getters for the known used GitHub api links
    Added methods for getting the JSON response from known used GitHub API calls

--- githubAPI.py
+++ githubAPI.py
@@ -9,8 +9,13 @@ class GitHubAPI:
         self.token = token
         self.cursor = cursor
         self.connection = connection
-        
+        self.headers = {"Authorization": "token " + self.token}
         self.baseURL = "https://api.github.com/repos/" + self.username + "/" + self.repository
+        
+        # These are static
+        self.commitsURL = self.baseURL + "/commits?state=all"
+        self.issuesURL = self.baseURL + "/issues?state=all"
+        self.pullsURL = self.baseURL + "/pulls?state=all"
 
     def set_Username(self, username:str=None):
         self.username = username
@@ -50,10 +55,35 @@ class GitHubAPI:
     def get_BaseURL(self):
         return self.baseURL
     
+    def set_Headers(self, token:str=None):  # This is the preferred method for updating the token
+        if token is not None:
+            self.token = token
+        self.headers = {"Authorization": "token " + self.token}
+
+    def get_Headers(self):
+        return self.headers
+
     def update_Instance(self, username:str=None, repository:str=None, token:str=None, cursor:Cursor=None, connection:Connection=None):
         self.set_Username(username=username)
         self.set_Repository(repository=repository)
         self.set_Token(token=token)
         self.set_Cursor(cursor=cursor)
         self.set_Connection(connection=connection)
-        
+        self.set_Headers(token=token)
+
+    def get_CommitsJSON(self):
+        foo = requests.get(url=self.commitsURL, headers=self.headers)
+        return foo.json()
+
+    def get_IssuesJSON(self):
+        foo = requests.get(url=self.issuesURL, headers=self.headers)
+        return foo.json()
+
+    def get_PullsJSON(self):
+        foo = requests.get(url=self.pullsURL, headers=self.headers)
+        return foo.json()
+
+    def get_GitHubAPIJSON(self, url:str=None, headers:dict=None):
+        foo = requests.get(url=url, headers=headers)
+        return foo.json()
+    
