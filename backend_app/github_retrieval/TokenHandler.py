09063495182782f4501d9157d8cc750806ab6701
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-02-17 16:08:57 -0600

    Add deleteValue function to remove unusable keys

--- backend_app/github_retrieval/TokenHandler.py
+++ backend_app/github_retrieval/TokenHandler.py
@@ -4,9 +4,15 @@ class TokenHandler:
         self.filename = "tokens.txt"
         open("tokens.txt", "w").close()  # Gaurentees creation of tokens.txt if it is not created
     
-    def write(self, token:str)  ->  None:
-        keysfile = open(self.filename, "a+")    # Opens file in appending mode
+    def write(self, token:str, mode:str="a+")  ->  None:
+        keysfile = open(self.filename, mode)    # Opens file in appending mode
         keysfile.writelines(token + "\n")
+        keysfile.close()
+
+    def writelines(self, data:list=None, mode:str="a+")    ->  None:
+        keysfile = open(self.filename, mode)
+        for item in data:
+            keysfile.write(item + "\n")
         keysfile.close()
 
     def read(self)    ->  list:
@@ -14,3 +20,15 @@ class TokenHandler:
         foo = keysfile.readlines()
         keysfile.close()
         return [x.replace("\n", "") for x in foo]
+
+    def deleteValue(self, value:str=None)   ->  None:
+        '''
+Deletes all instances of a value in the file.
+        '''
+        data = self.read()
+        for x in range(len(data)):
+            try:
+                data.remove(value)
+            except ValueError:
+                break
+        self.writelines(data=data, mode="w")
