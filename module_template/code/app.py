99583fd228caec85caf4c095def479e544ba0e3e
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-03-09 18:49:51 -0500

    Remove extranous files

--- module_template/code/app.py
+++
@@ -1,9 +0,0 @@
-import requests
-import sys
-
-print("Hello, World!")
-cliArg1 = sys.argv[1]
-print("Your first command line argument is: ")
-print(cliArg1)
-result = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(result.json())
