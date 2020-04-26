90f2ff87d50c2f50e7b610546d1128bbee856f71
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-04-26 12:08:08 -0500

    modified flaskr to take cl args

--- Graph/code/flaskr.py
+++ Graph/code/flaskr.py
@@ -1,5 +1,6 @@
 # all the imports
 import os
+import sys
 import sqlite3
 import pandas as pd
 from flask import Flask, g, render_template
@@ -11,15 +12,18 @@ app = Flask(__name__)
 # configuration
 app.config.update(dict(
     # temporarily testing SSLMetrics repo data
-    DATABASE='/metrics/SSLMetrics_commits.db',
+    DATABASE='/metrics/' + str(sys.argv[1]) + '.db',
 ))
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+
 def init_db():
     with closing(connect_db()) as db:
         db.commit()
+
 
 @app.route('/')
 def stacked_bar_chart():
@@ -32,7 +36,7 @@ def stacked_bar_chart():
 
     con.close()
 
-    date = df['date'].values.tolist() # x axis
+    date = df['date'].values.tolist()  # x axis
     commits = df['commits'].values.tolist()
     issues = df['issues'].values.tolist()
     lines_of_code = df['lines_of_code'].values.tolist()
@@ -41,12 +45,13 @@ def stacked_bar_chart():
     issue_spoilage_max = df['issue_spoilage_max'].values.tolist()
     defect_density = df['defect_density'].values.tolist()
 
-    return render_template('linegraph.html', date=date, commits=commits) #issues=issues, pull_requests=pull_requests)
+    return render_template('linegraph.html', date=date, commits=commits)  # issues=issues, pull_requests=pull_requests)
+
 
 if __name__ == '__main__':
     init_db()
     app.run(debug=True, host='0.0.0.0')
 
 # first build the docker image
-# then run 'docker run -v metrics:/metrics -p 5000:5000 <name_of_image>' 
+# then run 'docker run -v metrics:/metrics -p 5000:5000 <name_of_image>'
 # this opens a port for the flask server to run in
