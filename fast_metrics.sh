cc1234c32c1b22d368c983a7f149a3fab76dca9a
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-04-27 21:20:38 -0500

    added fast_metrics option to do the heavy lifting locally using git cloning

---
+++ fast_metrics.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo "Running all metrics scripts for repository: $1"
+
+docker volume create metrics
+
+# Run GitModule
+cd GitModule
+./metrics.sh $1
+cd ..
+
+# Run Issues
+cd Issues
+./metrics.sh $1 $2
+cd ..
+
+# Run Issue_Spoilage
+cd Issue_Spoilage
+./metrics.sh $1 $2
+cd ..
+
+# Run Defect Density (this command should also copy volume content to the current dir)
+cd Defect_Density
+./metrics.sh $1 $2
+cd ..
+
+# Run Graph
+cd Graph
+./metrics.sh $1
+cd ..
+
+
+
