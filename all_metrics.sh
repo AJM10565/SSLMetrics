6c3e8a4181382f5989dbd5e04adb536ac9fdd19d
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-05-01 11:44:22 -0500

    deprecating all_metrics

--- all_metrics.sh
+++ all_metrics.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env bash
+echo "This script has been deprecated use fast_metrics.sh"
 
-echo "Running all metrics scripts for repository: $1"
+#echo "Running all metrics scripts for repository: $1"
 
-docker volume create metrics
+#docker volume create metrics
 
 # Run Commits
-cd Commits
-./metrics.sh $1 $2
-cd ..
+#cd Commits
+#./metrics.sh $1 $2
+#cd ..
 
 # Run Issues
-cd Issues
-./metrics.sh $1 $2
-cd ..
+#cd Issues
+#./metrics.sh $1 $2
+#cd ..
 
 # Run Lines of Code
-cd Lines_Of_Code_Num_Of_Chars
-./metrics.sh $1 $2
-cd ..
+#cd Lines_Of_Code_Num_Of_Chars
+#./metrics.sh $1 $2
+#cd ..
 
 # Run Issue_Spoilage
-cd Issue_Spoilage
-./metrics.sh $1 $2
-cd ..
+#cd Issue_Spoilage
+#./metrics.sh $1 $2
+#cd ..
 
 # Run Defect Density (this command should also copy volume content to the current dir)
-cd Defect_Density
-./metrics.sh $1 $2
-cd ..
+#cd Defect_Density
+#./metrics.sh $1 $2
+#cd ..
 
 # Run Graph
-cd Graph
-./metrics.sh $1
-cd ..
+#cd Graph
+#./metrics.sh $1
+#cd ..
 
 
 
